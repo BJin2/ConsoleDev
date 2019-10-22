e5c79cfc5fe9cf9c3c6681383d25e999587ee4e7
Jin <bomjan@naver.com>
2019-10-22 16:34:06 -0400

    Created tracker material

--- Source/Week01/Tracker.h
+++ Source/Week01/Tracker.h
@@ -19,7 +19,7 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Tracker Properties")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Tracker Properties")
 	UStaticMeshComponent* MeshComp;
 
 public:	
